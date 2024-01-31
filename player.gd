6946cfa1b3f8e3f5f0ae1d0f3a7dd7a126e2f0d1
olive <50035022+k-onej@users.noreply.github.com>
2024-01-31 09:51:25 -0800

    made player more better

--- player.gd
+++ player.gd
@@ -1,23 +1,27 @@
-# AWESOME DEVELOPER NOTE TO SELF: THE THING WHERE YOU CAN SLIDE ON WALLS IS INTENTIONAL.
-# I AM SURE THERE IS AN EASY FIX FOR IT BUT I THINK IT'S COOL SO IDC. :3
-
 extends CharacterBody2D
 
 
-@export var speed := 1000
-@export var accel := 1400
+@export var max_speed := 1000
+@export var accel := 300
+@export var decel := 0.4
 @export var gravity := 70000
 @export var max_fall_speed := 120000
 
 @onready var spawn_position = position
 
+@onready var sprite = $Sprite2D
+
 func _physics_process(delta):
-	# Add the gravity.
+	print(velocity.x)
+	# Add the gravity, gravity direction changes based on up direction
+	# BUG: player slides slowly and is_on_floor() returns true when switching gravity while moving against wall
 	if not is_on_floor():
 		if up_direction == Vector2.UP:
 			velocity.y = min(velocity.y + gravity * delta, max_fall_speed * delta)
+			sprite.flip_v = false
 		if up_direction == Vector2.DOWN:
 			velocity.y = max(velocity.y - gravity * delta, -max_fall_speed * delta)
+			sprite.flip_v = true
 
 	# Handle switching gravity.
 	if Input.is_action_just_pressed("switch_gravity") and is_on_floor():
@@ -29,10 +33,14 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
+	if direction > 0:
+		velocity.x = min(velocity.x + accel * direction, max_speed)
+		sprite.flip_h = false
+	elif direction < 0:
+		velocity.x = max(velocity.x + accel * direction, -max_speed)
+		sprite.flip_h = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = lerp(velocity.x, 0.0, decel)
 	
 	move_and_slide()
 	
